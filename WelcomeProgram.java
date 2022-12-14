3cbfefdc9752c66f5db76349c46b7da162842da9
shraddha0930 <shraddhaingole11@gmail.com>
2022-12-14 15:17:33 +0530

    passed welcome  message

---
+++ WelcomeProgram.java
@@ -0,0 +1,7 @@
+package day8Practiceproblem.LineComparisonUsingOOPS;
+
+public class WelcomeProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
